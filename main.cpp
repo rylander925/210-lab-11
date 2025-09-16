85ab5450a149bc957619a0517a61d6dc902797ed
rylander925 <rylander925@gmail.com>
2025-09-16 12:55:53 -0700

    adding fill fucntion

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ IDE Used: Visual Studio Code
 */
 
 #include<iostream>
+#include <string>
 #include<fstream>
 
 using namespace std;
@@ -19,7 +20,8 @@ struct Playlist {
     string name;
     string genre;
     Song* songs;
-    Playlist(): size(0), name(""), genre(""), songs(nullptr) { }
+    Playlist(): size(-1), name(""), genre(""), songs(nullptr) { }
+    void Fill(istream* input);
     void Display();
     ~Playlist() {
         if (songs) {
@@ -29,20 +31,44 @@ struct Playlist {
     }
 };
 
-Playlist inputPlaylist();
-
-void fillPlaylistlist(Playlist* playlistlist, int size);
+void fillPlaylistlist(Playlist* playlistlist, int size, string filename);
 
 void displayPlaylistlist(Playlist* playlistlist, int size);
 
 int main() {
+}
+
+void Playlist::Fill(istream* input) {
+    cout << "Enter name: " << endl;
+    getline(*input, name);
+    cout << "Enter genre: " << endl;
+    getline(*input, genre);
+    cout << "Enter playlist size: " << endl;
+    while(size < 0) { //allow empty playlists, size is -1 by default
+        while(!(*input >> size)) { 
+            cout << "Size must be an integer" << endl;
+            input->clear();
+            input->ignore(100, '\n');
+        }
+        cout << "Size must be non-negative" << endl;
+    }
+    
+    
+
+} 
+
+void Playlist::Display() {
 
 }
 
-void fillPlaylistlist(Playlist* playlistlist, int size) {
+void fillPlaylistlist(Playlist* playlistlist, int size, string filename) {
+    istream* input;
+    //TODO: Add file input
+    input = &cin;
     cout << "Filling playlist list" << endl;
     for(int i = 0; i < size; i++) {
-        *(playlistlist + i) = inputPlaylist();
+        cout << "Playlist #" << i + 1 << ":" << endl;
+        (playlistlist + i)->Fill(input);
         
     }
 }
