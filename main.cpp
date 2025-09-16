c13654e9159962bc03bb52aa78c6b75a2fd7d9a4
rylander925 <rylander925@gmail.com>
2025-09-16 12:40:19 -0700

    update

--- main.cpp
+++ main.cpp
@@ -15,11 +15,12 @@ struct Song {
 };
 
 struct Playlist {
-    Playlist();
     int size;
     string name;
     string genre;
     Song* songs;
+    Playlist(): size(0), name(""), genre(""), songs(nullptr) { }
+    void Display();
     ~Playlist() {
         if (songs) {
             delete [] songs;
@@ -28,11 +29,27 @@ struct Playlist {
     }
 };
 
-void fillPlaylistlist(Playlist* playlistlist);
+Playlist inputPlaylist();
 
-void displayPlaylistlist(Playlist* playlistlist);
+void fillPlaylistlist(Playlist* playlistlist, int size);
+
+void displayPlaylistlist(Playlist* playlistlist, int size);
 
 int main() {
 
 }
 
+void fillPlaylistlist(Playlist* playlistlist, int size) {
+    cout << "Filling playlist list" << endl;
+    for(int i = 0; i < size; i++) {
+        *(playlistlist + i) = inputPlaylist();
+        
+    }
+}
+
+void displayPlaylistlist(Playlist* playlistlist, int size) {
+    cout << "Display Playlists" << endl;
+    for(int i = 0; i < size; i++) {
+        (playlistlist + i)->Display();
+    }
+}
