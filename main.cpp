0202d75cea550009b06fb898ee51c2706db03c64
rylander925 <rylander925@gmail.com>
2025-09-16 12:30:16 -0700

    added structs and function stubs

--- main.cpp
+++ main.cpp
@@ -2,3 +2,37 @@
 COMSC-210 | Lab 11 | Rylan Der
 IDE Used: Visual Studio Code
 */
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+struct Song {
+    string name;
+    int duration; //in seconds
+    Song(string name, int duration): name(name), duration(duration) { }
+};
+
+struct Playlist {
+    Playlist();
+    int size;
+    string name;
+    string genre;
+    Song* songs;
+    ~Playlist() {
+        if (songs) {
+            delete [] songs;
+        }
+        songs = nullptr;
+    }
+};
+
+void fillPlaylistlist(Playlist* playlistlist);
+
+void displayPlaylistlist(Playlist* playlistlist);
+
+int main() {
+
+}
+
